0ecda6737d35b4a805d7ae2d93f374a98bbd3fc2
Thomas Taschauer <tomtasche@gmail.com>
2013-07-12 21:58:11 +0200

    raise targetSdkVersion, use Holo theme

--- src/com/xabber/android/utils/DummyCursor.java
+++ src/com/xabber/android/utils/DummyCursor.java
@@ -209,4 +209,10 @@ public class DummyCursor implements Cursor {
 		return null;
 	}
 
+	@Override
+	public int getType(int columnIndex) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
